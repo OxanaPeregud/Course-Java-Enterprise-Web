2b133b035ef5625d882f479f2db3440adf90b0ae
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-06-17 12:15:23 +0300

    fix: mockito test

--- src/test/java/com/peregud/studentdao/dao/StudentDAOImplTest.java
+++ src/test/java/com/peregud/studentdao/dao/StudentDAOImplTest.java
@@ -3,38 +3,35 @@ package com.peregud.studentdao.dao;
 import com.peregud.studentdao.dao.impl.StudentDAOImpl;
 import com.peregud.studentdao.model.Student;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
-class StudentDAOImplTest {
+public class StudentDAOImplTest {
     Student student1 = new Student();
     Student student2 = new Student();
     List<Student> studentList = List.of(student1, student2);
 
     @Test
-    void getStudents() {
+    public void getStudents() {
         StudentDAOImpl studentDAO = mock(StudentDAOImpl.class);
         when(studentDAO.getAll()).thenReturn(studentList);
         assertEquals(studentList, studentDAO.getAll());
     }
 
     @Test
-    void addStudent() {
+    public void addStudent() {
         StudentDAOImpl studentDAO = mock(StudentDAOImpl.class);
         when(studentDAO.add(student2)).thenReturn(student2);
         assertEquals(student2, studentDAO.add(student2));
     }
 
     @Test
-    void getStudentById() {
+    public void getStudentById() {
         StudentDAOImpl studentDAO = mock(StudentDAOImpl.class);
         when(studentDAO.getById(any(Integer.class))).thenReturn(student1);
         assertEquals(student1, studentDAO.getById(1));
